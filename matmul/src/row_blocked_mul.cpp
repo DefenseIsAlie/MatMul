35a6a1c20b9bcb47c7c20d24a47ed4fe85515615
s200010021 <s200010021@cares2.iitdh.ac.in>
2022-09-15 13:41:39 +0530

    changed matmul3

--- matmul/src/row_blocked_mul.cpp
+++ matmul/src/row_blocked_mul.cpp
@@ -29,27 +29,24 @@ void row_block::_initialize_matrix(int dim, double** &mat){
 
 void row_block::_matmul(int dim,int block_size, double** &A, double** &B, double** &C){     
 
-    for (int i = 0; i < dim; i+=block_size){
+    for (int block = 0; block < dim; block+=block_size){
 
-        for (int block = 0; block < block_size; block++){
-        // i+block gives the row we are working on
+        // Read Ci and Ai into fast memory
 
-            for (int j = 0; j < dim; j++)
-            {
-                // Cij += RAi * CBj
-                double dotprod = 0.;
+        for (int  element = 0; element < dim; element++){
 
-                for (int o = 0; o < dim; o++)
-                {
-                    dotprod += A[i+block][o]*B[o][j];
-                    
+            for (int block_row = 0; block_row < block_size; block_row++){
+
+                for (int dot = 0; dot < dim; dot++){
+
+                    C[block+block_row][element] += A[block+block_row][element] * B[dot][element];
                 }
 
-                C[i+block][j] += dotprod;
+                
             }
-
+            
         }
-
+    
     }
 
 }
